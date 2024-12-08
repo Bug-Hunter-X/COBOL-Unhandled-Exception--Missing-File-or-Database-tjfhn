b78161effadbc289dc5f57f0d481f29a19d38041
BugHunter-X <askzenai@gmail.com>
2024-12-08 05:24:05 +0100

    Add bug.cob file

---
+++ bug.cob
@@ -0,0 +1,7 @@
+0000-0000-0000-0000
+       PROCEDURE DIVISION.
+           DISPLAY "Hello, World!".
+           STOP RUN.
+
+       * An unhandled exception could occur if the program tries to access a file or database
+       * that does not exist. In a production environment, this will result in the program crashing.
